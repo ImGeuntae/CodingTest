666e839f27c84e6d75b7e994e57ad2f69d9f6e54
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-19 22:18:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.88 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,6 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(participant)):
+        if i == len(participant)-1 or participant[i] != completion[i]:
+            return participant[i]
