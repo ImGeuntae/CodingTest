1952f1e6e46d48e622aff09e88cc7450b65d1bd0
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-21 13:27:49 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,6 @@
 def solution(citations):
     citations.sort(reverse=True)
-    answer = -1
     for idx, i in enumerate(citations):
         if (i < (idx+1)):
-            answer = idx
-            break
-    if answer == -1:
-        answer = idx+1
-    return answer
+            return idx
+    return idx+1
