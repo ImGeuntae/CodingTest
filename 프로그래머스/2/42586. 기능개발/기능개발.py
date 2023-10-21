d43f06421baf4644107c5e0dfb48996705a4b6fd
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-21 20:41:53 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,18 +1,10 @@
 def solution(progresses, speeds):
-    l = []
-    for i in range(len(progresses)):
-        num = (100 - progresses[i])/speeds[i]
-        if int(num) != num:
-            num = int(num)+1
-        l.append(num)
     answer = []
-    day = l[0]
-    count = 1
-    for j in range(1,len(l)):
-        if l[j] > day:
-            day = l[j]
-            answer.append(count)
-            count = 0
-        count += 1
-    answer.append(count)
+    for i in range(len(progresses)):
+        day = -((progresses[i]-100)//speeds[i])
+        if (i == 0) or (day > release):
+            release = day
+            answer.append(1)
+        else:
+            answer[-1] += 1
     return answer
