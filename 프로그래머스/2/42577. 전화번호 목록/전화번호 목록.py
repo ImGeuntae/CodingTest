4433e44748291d8b47b31f745c8b86014e81073d
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-26 13:57:00 +0900

    [level 2] Title: 전화번호 목록, Time: 222.25 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,7 @@
 def solution(phone_book):
-    dic = {}
-    for i in phone_book:
-        dic[i] = 1
+    hash_map = dict.fromkeys(phone_book)
     for j in phone_book:
         for k in range(len(j)-1):
-            if j[:k+1] in dic:
+            if j[:k+1] in hash_map:
                 return False
     return True
