f499ebbd76947d05231c63219f265fea1b85c98e
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-26 13:17:22 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,18 @@
 def solution(participant, completion):
     h = sum([hash(i) for i in participant]) - sum([hash(j) for j in completion])
     return [k for k in participant if hash(k)==h][0]
-    
+
+
+
+
+
+# hash 미사용
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(participant)):
+        if i == len(participant)-1 or participant[i] != completion[i]:
+            return participant[i]
+
+# 메모리: 26.2 MB, 시간: 72.88 ms
