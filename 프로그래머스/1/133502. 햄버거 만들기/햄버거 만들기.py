1183fee251e920215d9e7315d0c1882193060798
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-27 11:14:12 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,7 +3,7 @@ def solution(ingredient):
     answer = 0
     for i in ingredient:
         L.append(i)
-        if len(L)>=4 and L[-4:]==[1,2,3,1]:
+        if L[-4:]==[1,2,3,1]:
             answer += 1
             del L[-4:]
     return answer
