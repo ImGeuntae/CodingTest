8ce996269caa2a1a7235ed0045c247585652cd67
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-27 13:04:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,9 +1,2 @@
 def solution(s):
-    if len(s) == 4 or len(s) == 6:
-        try:
-            a = int(s)
-            return True
-        except:
-            return False
-    else:
-        return False
+    return s.isdigit() and len(s) in [4,6]
