ddbb8a56af2176a3e97fe3516bfe89162c1f7b24
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-31 20:37:27 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,10 @@
 def solution(priorities, location):
-    que = list(range(len(priorities)))
-    answer = 1
+    answer = 0
+    search = sorted(priorities, reverse=True)
     while True:
-        n = priorities[0]
-        if [x for x in priorities[1:] if x>n]:
-            priorities.append(n)
-            que.append(que[0])
-            priorities.pop(0)
-            que.pop(0)
-        else:
-            if que[0]==location:
-                return answer
-                break
-            priorities.pop(0)
-            que.pop(0)
-            answer += 1
+        for i, priority in enumerate(priorities):
+            s = search[answer]
+            if priority == s:
+                answer += 1
+                if i == location:
+                    return answer
