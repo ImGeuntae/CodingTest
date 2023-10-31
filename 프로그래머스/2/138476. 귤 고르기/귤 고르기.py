50aebe5e96496c35c3be5e91d8be479e1becf54c
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-31 20:16:07 +0900

    [level 2] Title: 귤 고르기, Time: 50.45 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -2,9 +2,9 @@ def solution(k, tangerine):
     dic = dict.fromkeys(tangerine,0)
     for i in tangerine:
         dic[i] += 1
-    L = sorted(dic,key=lambda x:dic[x])
-    for _ in range(len(tangerine)-k):
-        dic[L[0]] -= 1
-        if dic[L[0]] == 0:
-            L.pop(0)
-    return len(L)
+    L = sorted(dic,key=lambda x:dic[x],reverse=True)
+    j = 0
+    while k>0:
+        k -= dic[L[j]]
+        j += 1
+    return j
