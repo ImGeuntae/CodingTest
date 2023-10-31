55309601d4657d68edb0008a66e14a24aab90014
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-10-31 16:53:34 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,13 +1,8 @@
-G = ["D0","D+","C0","C+","B0","B+","A0","A+"]
-S1 = S2 = float(0)
+S1 = S2 = 0
 for _ in range(20):
     *others, n, g = input().split()
-    if g == "P":
-        continue
-    elif g == "F":
-        s = 0
-    else:
-        s = (0.5)*((G.index(g))+2)
-    S1 += s*float(n)
-    S2 += float(n)
+    if g != "P":
+        g = ("FDCBA".find(g[0]))+(0.5*(g[-1]=="+"))
+        S1 += g*float(n)
+        S2 += float(n)
 print(S1/S2)
