ec0078fddcc5adf38b15463499272abd55034400
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-01 19:21:09 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 114.23 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,10 +1,10 @@
 def solution(numbers):
     answer = []
     for i in numbers:
-        x = "0"+str(format(i, 'b'))
-        if x[-1]=="0":
-            answer.append(i+1)
-        else:
+        if i%2:
+            x = "0"+str(format(i, 'b'))
             idx = x.rfind("01")
             answer.append(int(x[:idx]+"10"+x[idx+2:],2))
+        else:
+            answer.append(i+1)
     return answer
