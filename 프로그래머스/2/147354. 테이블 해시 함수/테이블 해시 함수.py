544c5ba07a10438292791a32babea413301b3de3
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-01 16:18:25 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,7 +1,7 @@
 def solution(data, col, row_begin, row_end):
     data.sort(key=lambda x:(x[col-1],-x[0]))
     A = [sum([n%i for n in data[i-1]]) for i in range(row_begin,row_end+1)]
-    answer = A[0]
-    for i in range(len(A)-1):
-        answer = answer ^ A[i+1]
+    answer = 0
+    for i in A:
+        answer = answer ^ i
     return answer
