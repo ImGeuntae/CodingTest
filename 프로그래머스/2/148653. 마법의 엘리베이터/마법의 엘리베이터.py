1b11f74d84611560dd4f3387cd0e7cfb185dbede
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-10 18:59:02 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,17 @@
+def solution(storey):
+    ans = c = a = 0
+    for i in str(storey)[::-1]:
+        i = int(i) + c
+        if i < 5:
+            ans += i
+            c = 0
+            a = 0
+        elif i > 5 or (i==5 and a):
+            ans += 10-i-a
+            c = 1
+            a = 0
+        else:
+            ans += i
+            c = 0
+            a = 1           
+    return ans + c
