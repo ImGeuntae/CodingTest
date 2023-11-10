98ccbf603eaebd279b5762ae0f8815c0aabc0dc4
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-10 19:54:15 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,13 +1,9 @@
 def solution(n, k):
-    N = [n for n in range(1,n+1)]
-    f = 1
-    ans = []
+    f, N, ans = 1, [n for n in range(1,n+1)], []
     for i in range(1,n):
         f *= i
     k -= 1
-    while i:
+    for i in range(n-1,0,-1):
         ans.append(N.pop(k//f))
-        k %= f
-        f //= i
-        i -= 1
+        k, f = k%f, f//i
     return ans+N
