a4107194a8abcdb47275c7f8a05d5908df4fa873
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-10 21:57:43 +0900

    [level 2] Title: 하노이의 탑, Time: 28.58 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,5 +1,4 @@
 def Hanoi(n,a,b,c):
-    return [a,b] if n == 1 else Hanoi(n-1,a,c,b)+[a,b]+Hanoi(n-1,c,b,a)
+    return Hanoi(n-1,a,c,b) + [[a,b]] + Hanoi(n-1,c,b,a) if n else []
 def solution(n):
-    H = Hanoi(n,1,3,2)
-    return [[x,y] for x,y in zip(H[0::2],H[1::2])]
+    return Hanoi(n,1,3,2)
