d71ef8284ec1de85030b90ae697e0f3f7326bb3f
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-11 11:14:01 +0900

    [level 2] Title: 점 찍기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,5 +1,3 @@
 def solution(k, d):
-    ans = 0
-    for i in range(0,d+1,k):
-        ans += int((((d**2)-(i**2))**0.5)//k) + 1
-    return ans
+    return sum([int((((d**2)-(i**2))**0.5)//k)+1 for i in range(0,d+1,k)])
+    
