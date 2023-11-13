e0ba3b164e8942b0d4ae1236d2e5f6fc1b14bef4
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-13 16:27:08 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,16 @@
 def solution(n, computers):
-    C = {x for x in range(n)}
+    C = [x for x in range(n)]
     answer = 0
     while C:
-        visited = set()
-        queue = list()
-        queue.append(list(C)[0])
+        visited = []
+        queue = []
+        queue.append(C[0])
         while queue:
             node = queue.pop(0)
             if node not in visited:
-                visited.add(node)
+                visited.append(node)
                 queue.extend([idx for idx, i in enumerate(computers[node]) if i==1 and idx!=node])
         answer += 1
-        C -= visited
+        C = [x for x in C if x not in visited]
     return answer
         
