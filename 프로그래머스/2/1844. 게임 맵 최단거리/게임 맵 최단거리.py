c12b2d98b9e4d2ab9640fff3bdb0a96033426408
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-13 15:06:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,3 +1,12 @@
 def solution(maps):
-    answer = 0
-    return answer
+    n,m = len(maps),len(maps[0])
+    queue = [(0,0)]
+    d = [(1,0),(-1,0),(0,1),(0,-1)]
+    while queue:
+        x,y = queue.pop(0)
+        for i in range(4):
+            xx,yy = x+d[i][0], y+d[i][1]
+            if 0<=xx<=n-1 and 0<=yy<=m-1 and maps[xx][yy] == 1:
+                maps[xx][yy] += maps[x][y]
+                queue.append((xx,yy))
+    return maps[n-1][m-1] if maps[n-1][m-1] != 1 else -1
