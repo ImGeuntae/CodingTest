96a1e9f6708629e5af45afe9b99a42a8079d9186
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-13 21:01:36 +0900

    Update 타겟 넘버.py

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -7,3 +7,20 @@ def solution(numbers, target):
             nn.append(n - num)
         N = nn
     return N.count(target)
+
+###########################################
+
+from collections import defaultdict as dd
+def solution(numbers, target):
+    N = [0]
+    D = dd(int)
+    D[0] = 1
+    for num in numbers:
+        s,d = set(), dd(int)
+        for n in N:
+            s.add(n+num)
+            s.add(n-num)
+            d[n+num] += D[n]
+            d[n-num] += D[n]
+        N,D = s,d
+    return D[target]
