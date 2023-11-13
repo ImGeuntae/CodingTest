c3422603f2744a1b58cbdcd4bf6dced341c68a40
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-13 14:54:50 +0900

    [level 2] Title: 타겟 넘버, Time: 0.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,14 @@
+from collections import defaultdict as dd
 def solution(numbers, target):
     N = [0]
+    D = dd(int)
+    D[0] = 1
     for num in numbers:
-        nn = []
+        s,d = set(), dd(int)
         for n in N:
-            nn.append(n + num)
-            nn.append(n - num)
-        N = nn
-    return N.count(target)
+            s.add(n+num)
+            s.add(n-num)
+            d[n+num] += D[n]
+            d[n-num] += D[n]
+        N,D = s,d
+    return D[target]
