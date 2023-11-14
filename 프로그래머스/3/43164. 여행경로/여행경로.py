9d41900447a625b5bc69cca5691c9acaccf210d7
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-14 15:51:40 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,19 @@
+from collections import defaultdict as dd
+def solution(tickets):
+    D = dd(list)
+    for a,b in sorted(tickets):
+        D[a].append(b)
+    return ["ICN"] + DFS("ICN",D,len(tickets))
+
+def DFS(leave,T,n):
+    if n:
+        for i in range(len(T[leave])):
+            arrive = T[leave].pop(0)
+            temp = DFS(arrive,T,n-1)
+            if temp != 0:
+                return [arrive] + temp
+            T[leave].append(arrive)
+        else:
+            return 0
+    else:
+        return []
