d3c7acc87f37963d70980828a8459b00c5c7aea3
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-21 19:13:08 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 445.75 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,5 +1,5 @@
 def solution(sequence):
-    A, B = [0], [0] 
+    A, B = [float("-inf")],[float("-inf")]
     for a,b in [(n,-n) if (idx%2) else (-n,n) for idx,n in enumerate(sequence)]:
         A.append(max(a,a+A[-1]))
         B.append(max(b,b+B[-1]))
