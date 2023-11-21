d8d483699294abe952d3a45fcf0350774eb9e244
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-21 16:07:47 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.42 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,7 +1,6 @@
 def solution(m, n, board):
     B = list(map(list,(zip(*[list(b) for b in board][::-1]))))
     bomb = True
-    ans = 0
     while bomb:
         bomb = set()
         for i in range(n-1):
@@ -13,5 +12,4 @@ def solution(m, n, board):
                     continue
         for (x,y) in sorted(list(bomb), key=lambda x:-x[1]):
             B[x].pop(y)
-            ans += 1
-    return ans
+    return m*n - sum([len(x) for x in B])
