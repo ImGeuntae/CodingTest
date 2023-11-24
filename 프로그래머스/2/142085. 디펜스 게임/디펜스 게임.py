878878b5db7897bc258d37ecf519edc1544c867b
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-24 22:41:26 +0900

    [level 2] Title: 디펜스 게임, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,13 +1,9 @@
 import heapq
 def solution(n, k, enemy):
-    if len(enemy) < k:
-        return len(enemy)
-    ans,heap = k,enemy[:k]
+    heap = enemy[:k]
     heapq.heapify(heap)
-    for i in enemy[k:]:
-        n -= heapq.heappushpop(heap,enemy[ans])
+    for i in range(k,len(enemy)):
+        n -= heapq.heappushpop(heap,enemy[i])
         if n<0:
-            break
-        else:
-            ans += 1
-    return ans
+            return i
+    return len(enemy)
