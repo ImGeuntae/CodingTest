4e46bff75a39782145343eb75b6ed6de0605bbbd
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-27 13:24:18 +0900

    [level 4] Title: 징검다리, Time: 94.54 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,28 +1,18 @@
 def solution(distance, rocks, n):
     rocks.sort()
+    rocks.append(distance)
     left, right = 1,distance
     while left <= right:
-        N,now,middle = n,0,(left+right)//2
-        R = []
+        N,now,mid = n,0,(left+right)//2
         for r in rocks:
-            if r-now < middle:
+            if r-now < mid:
                 N -= 1
                 if N < 0:
-                    right = middle - 1
+                    right = mid - 1
                     break
             else:
                 now = r
-                R.append(r)
         else:
-            now  = distance
-            for r in R[::-1]:
-                if now-r < middle:
-                    N -= 1
-                else:
-                    break
-            if N < 0:
-                right = middle - 1
-            else:
-                left = middle + 1
-                ans = middle 
+            left = mid + 1
+            ans = mid
     return ans
