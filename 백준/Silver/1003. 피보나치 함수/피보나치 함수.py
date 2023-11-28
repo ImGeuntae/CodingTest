d1e880c26b3c54f98aca2803f7d6631554521d72
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-28 18:53:55 +0900

    [Silver III] Title: 피보나치 함수, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,9 +1,5 @@
 for _ in range(int(input())):
-    n = int(input())
-    if n:
-        a0,a1,b0,b1 = 1,0,0,1
-        for _ in range(2,n+1):
-            a0,a1,b0,b1 = b0,b1,a0+b0,a1+b1
-        print(b0,b1)
-    else:
-        print(1,0)
+    a,b = 1,0
+    for _ in range(int(input())):
+        a,b = b,a+b
+    print(a,b)
