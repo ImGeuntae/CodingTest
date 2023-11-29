4f4ae4c96a670eb6868b06f3f0f55862c397d1f2
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-29 18:48:56 +0900

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,5 +1,5 @@
 for _ in range(int(input())):
-    L = [0,0,1,0,1]
+    a=b=c=1
     for _ in range(int(input())-1):
-        L.append(L.pop(0)+L[-1])
-    print(L[-1])
+        a,b,c=b,c,a+b
+    print(a)
