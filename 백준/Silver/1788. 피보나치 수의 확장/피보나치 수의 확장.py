2d2df4b1e1e92878439a62f8f3ddb54cc7e739f8
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-11-29 22:53:35 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -1,9 +1,5 @@
 n,a,b = int(input()),1,0
-if n>0:
-    for _ in range(0,n):
-        a,b = b,(a+b)%1000000000
-elif n<0:
-    for _ in range(0,n,-1):
-        a,b = b,(abs(a-b)%1000000000)*([-1,1][(a>0)])
-print(-1*(b<0)+(b>0))
-print(abs(b))
+print((n!=0)*(2*(n>0 or n%2)-1))
+for _ in range(abs(n)):
+    a,b = b,(a+b)%1000000000
+print(b)
