c00a402c952473b4744f831dd7eb4eca46086b22
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-02 17:54:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.24 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,14 +1,14 @@
 from collections import deque
 def solution(n, edge):
-    D = {k: [] for k in range(1,n+1)}    
+    D = [[] for _ in range(n+1)]
     for a,b in edge:
         D[a].append(b)
         D[b].append(a)
-    queue,F = deque([1]), [-1 if x else 0 for x in range(n)]
+    queue,F = deque([1]), [0 if x==1 else -1 for x in range(n+1)]
     while queue:
         q = queue.popleft()
         for x in D[q]:
-            if F[x-1] == -1:
-                F[x-1] = F[q-1] + 1
+            if F[x] == -1:
+                F[x] = F[q] + 1
                 queue.append(x)
     return F.count(max(F))
