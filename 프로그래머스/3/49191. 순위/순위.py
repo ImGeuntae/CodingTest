0c6535be642ec6ae17d6aba893cb3793fbd8b0b3
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-02 19:47:39 +0900

    [level 3] Title: 순위, Time: 9.89 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -4,15 +4,13 @@ def solution(n, results):
     for a,b in results:
         win[a].add(b)
         lose[b].add(a)
-    for x in 2*list(range(1,n+1)):
-        for w in win[x]:
-            for ww in win[w]:
-                lose[ww].add(x)
-        for l in lose[x]:
-            for ll in lose[l]:
-                win[ll].add(x)
+    for x in range(1,n+1):
+        for w in lose[x]:
+            win[w].update(win[x])
+        for l in win[x]:
+            lose[l].update(lose[x])
     ans = 0
-    for w,l in zip(win,lose):
-        if len(w) + len(l) == n-1:
+    for i in range(1, n+1):
+        if len(win[i]) + len(lose[i]) == n-1:
             ans += 1
     return ans
