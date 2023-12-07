dc414a3fed8df06cf69dbf3cd96b1f141bd461e3
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-07 21:03:33 +0900

    [Silver II] Title: 상자넣기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.py
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -1,7 +1,5 @@
-n = int(input())
-box = [0]+list(map(int,input().split()))
-dp,ans = [0]+[1]*n,0
-for i in range(1,n+1):
-    dp[i] += max(dp[j] for j in range(i-1,-1,-1) if box[i] > box[j])
-    ans = max(ans,dp[i])
-print(ans)
+d=[0]*1001
+input()
+for n in map(int,input().split()):
+    d[n]=max(d[:n])+1
+print(max(d))
