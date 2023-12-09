56839ee630cf0a16280d19138837bdcd58ce992d
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-09 23:43:46 +0900

    [Silver II] Title: 욱제가 풀어야 하는 문제, Time: 272 ms, Memory: 38756 KB -BaekjoonHub

--- 백준/Silver/18249. 욱제가 풀어야 하는 문제/욱제가 풀어야 하는 문제.py
+++ 백준/Silver/18249. 욱제가 풀어야 하는 문제/욱제가 풀어야 하는 문제.py
@@ -2,5 +2,6 @@ import sys
 dp,a,b,n = [0]*191230,0,1,10**9+7
 for i in range(191230):
     dp[i],a,b = b,b,(a+b)%n
-for _ in range(int(input())):
-    print(dp[int(sys.stdin.readline())])
+input()
+for n in sys.stdin:
+    print(dp[int(n)])
