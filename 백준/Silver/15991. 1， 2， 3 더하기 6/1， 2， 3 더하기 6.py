903dfc210eae51933113fb0d7a1cb72c3d6c7d12
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-14 20:45:32 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 252 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
@@ -1,7 +1,5 @@
-dp=[0]*(10**5+2)
-a,b,c=0,1,0
-for i in range(0,10**5+1,2):
-    a,b,c=b,c,a+b+c
-    dp[i]=dp[i+1]=c%(10**9+9)
+dp=[1,1,2,2,3,3]
+for i in range(6,10**5+1):
+    dp += [(dp[i-2]+dp[i-4]+dp[i-6])%(10**9+9)]
 for _ in range(int(input())):
     print(dp[int(input())])
