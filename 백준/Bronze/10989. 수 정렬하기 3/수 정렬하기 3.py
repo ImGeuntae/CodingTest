4a7efba8759e0ff3a10509aae5c7a3fe5e5fc989
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-17 18:14:20 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8984 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,8 +1,7 @@
 import sys
 L = [0] * 10001
 for _ in range(int(input())):
-    n = int(sys.stdin.readline())
-    L[n] += 1
+    L[int(sys.stdin.readline())] += 1
 for i in range(1,10001):
     if L[i]:
         for _ in range(L[i]):
