b272ef59ee09b335bdceb3497a9939ed170a18ef
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-18 17:30:41 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,12 +1,5 @@
 for _ in range(int(input())):
-    ans = 0
-    for x in input():
-        if x=='(':
-            ans += 1
-        else:
-            ans -= 1
-        if ans < 0:
-            print("NO")
-            break
-    else:
-        print("NO" if ans else "YES")
+    x = input()
+    while '()' in x:
+        x = x.replace('()','')
+    print('NO' if x else 'YES')
