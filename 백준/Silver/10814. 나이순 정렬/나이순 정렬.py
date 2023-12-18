634e2b7a8210b1f96548e9641180b3360431898b
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-18 23:22:17 +0900

    [Silver V] Title: 나이순 정렬, Time: 3788 ms, Memory: 40780 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,6 +1,3 @@
-L = []
-for _ in range(int(input())):
-    a,n = input().split()
-    L += [(int(a),n)]
-for i in sorted(L,key=lambda x:x[0]):
-    print(*i)
+L = [input() for _ in range(int(input()))]
+for i in sorted(L,key=lambda x:int(x.split()[0])):
+    print(i)
