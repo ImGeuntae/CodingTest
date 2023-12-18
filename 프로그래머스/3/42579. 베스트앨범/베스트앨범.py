f400bbb5a4b04c3b89ec7adc657e8d49aea2695f
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-18 16:09:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -3,6 +3,6 @@ def solution(genres, plays):
     D = dd(dict)
     for idx, (g,p) in enumerate(zip(genres, plays)):
         D[g][idx] = p
-    G = sorted(D, key=lambda x : sum((D[x]).values()), reverse=True)
+    G = sorted(D, key=lambda x : sum(D[x].values()), reverse=True)
     L = [sorted(D[i],key=lambda x : D[i][x], reverse=True)[:min(2,len(D[i]))] for i in G]
-    return [N for M in L for N in M]
+    return sum(L,[])
