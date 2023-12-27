cc026e947d2aaee8197d61d7db50af1c875b0e21
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-27 09:38:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,4 +1,2 @@
-n = input().split("-")
-a = sum(map(int,n[0].split('+')))
-b = sum(sum(map(int,x.split('+'))) for x in n[1:])
-print(a-b)
+a,*b = (sum(map(int,x.split('+'))) for x in input().split('-'))
+print(a-sum(b))
