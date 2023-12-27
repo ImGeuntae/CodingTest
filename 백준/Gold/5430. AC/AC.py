9d80402594d514473b9626a5bc8e6edf274d3347
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2023-12-27 10:20:11 +0900

    [Gold V] Title: AC, Time: 180 ms, Memory: 41584 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,15 +1,18 @@
 for _ in range(int(input())):
-    P,_,X = input(),input(),[n for n in input()[1:-1].split(',') if n]
-    r = False
+    P,n,X = input(),int(input()),[n for n in input()[1:-1].split(',') if n]
+    r,n = False,[0,n]
     for p in P:
         if p=='R':
             r = not r
-        elif X:
-            X.pop(-1*(r))
+        elif r:
+            n[1] -= 1
         else:
+            n[0] += 1
+        if n[0] > n[1]:
             print('error')
             break
     else:
+        X = X[n[0]:n[1]]
         if r:
             X = X[::-1]
         print('['+','.join(X)+']')
