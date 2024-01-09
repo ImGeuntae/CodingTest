d513e51528f3ddeb7bab56dbcc54f98734dfacce
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-01-09 09:44:13 +0900

    [Gold V] Title: 토마토, Time: 1852 ms, Memory: 98636 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,19 +1,13 @@
 from collections import deque
 M,N = map(int,input().split())
-T,Q,z = [],deque(),0
+T,Q = [],deque()
 for i in range(N):
-    T += [input().split()]
-    for j in range(M):
-        if T[i][j] == '1':
-            Q += [(i,j)]
-            T[i][j] = 0
-        elif T[i][j] == '0':
-            z += 1
+    T += [list(map(int,input().split()))]
+    Q += [(i,j) for j in range(M) if T[i][j]==1]
 while Q:
     x,y = Q.popleft()
     for a,b in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-        if 0<=a<N and 0<=b<M and T[a][b]=='0':
+        if 0<=a<N and 0<=b<M and T[a][b]==0:
             T[a][b] = T[x][y] + 1
-            z -= 1
             Q += [(a,b)]
-print(-1 if z else T[x][y])
+print(-1 if any(1 for t in T if 0 in t) else T[x][y]-1)
