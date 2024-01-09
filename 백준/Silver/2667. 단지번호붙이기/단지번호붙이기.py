8dbf5915d7ebc6f8652c717a86f94f691fbb3646
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-01-09 15:45:17 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,18 +1,17 @@
-from collections import deque
 N = int(input())
 M = [[*input()] for _ in range(N)]
-P,Q = [],deque()
+P = []
 for i in range(N):
     for j in range(N):
         if M[i][j]=='1':
-            p = 0
-            Q += [(i,j)]
+            P += [1]
+            Q = [(i,j)]
+            M[i][j] = '0'
             while Q:
-                x,y = Q.popleft()
+                x,y = Q.pop(0)
                 for a,b in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
                     if 0<=a<N and 0<=b<N and M[a][b]=='1':
-                        p += 1
+                        P[-1] += 1
                         M[a][b]='0'
                         Q += [(a,b)]
-            P += [p+(p==0)]
 print(len(P),*sorted(P),sep='\n')
