137c87f5119085ab71d4b80e00dfc0729dc87a5f
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-01-22 01:19:01 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,7 +1,5 @@
 def solution(s):
-    L = [i.split(",") for i in s[2:-2].split("},{")]
+    L = [set()] + [set(i.split(",")) for i in s[2:-2].split("},{")]
     L.sort(key = lambda x:len(x))
-    ans = []
-    for i in L:
-        ans.extend([int(n) for n in i if int(n) not in ans])
+    ans = [int((L[i]-L[i-1]).pop()) for i in range(1,len(L))]
     return ans
