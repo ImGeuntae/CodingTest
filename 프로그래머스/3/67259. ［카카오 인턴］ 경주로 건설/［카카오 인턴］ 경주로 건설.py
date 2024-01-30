f14f82105448855f870e1480fad4130459e2e91b
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-01-30 15:44:11 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -10,18 +10,19 @@ def solution(board):
                 BFS[2*i+1][2*j] = BFS[2*i-1][2*j] = BFS[2*i][2*j+1] = BFS[2*i][2*j-1] = -1
     Q = [[100,0,1],[100,1,0]]
     while Q:
-        d,a,b = heapq.heappop(Q)
+        c,a,b = heapq.heappop(Q)
         if a%2:
-            XY = [(a-2,b),(a+2,b),(a-1,b-1),(a+1,b+1),(a+1,b-1),(a-1,b+1)]
+            XY = [(a-2,b),(a+2,b)]
         else:
-            XY = [(a,b-2),(a,b+2),(a-1,b-1),(a+1,b+1),(a+1,b-1),(a-1,b+1)]
+            XY = [(a,b-2),(a,b+2)]
+        XY += [(a-1,b-1),(a+1,b+1),(a+1,b-1),(a-1,b+1)]
         for n,(x,y) in enumerate(XY):
             if 0<=x<(2*m-1) and 0<=y<(2*m-1) and BFS[x][y] != -1:
-                if n<2 and BFS[x][y] > d+100:
-                    BFS[x][y] = d+100
-                    heapq.heappush(Q,[d+100,x,y])
-                elif n>1 and BFS[x][y] > d+600:
-                    BFS[x][y] = d+600
-                    heapq.heappush(Q,[d+600,x,y])
+                if n<2 and BFS[x][y] > c+100:
+                    BFS[x][y] = c+100
+                    heapq.heappush(Q,[c+100,x,y])
+                elif n>1 and BFS[x][y] > c+600:
+                    BFS[x][y] = c+600
+                    heapq.heappush(Q,[c+600,x,y])
     ans = sorted([BFS[2*m-3][2*m-2],BFS[2*m-2][2*m-3]])
     return (ans[0] if ans[0] != -1 else ans[1])
