08a385bf18e8d49072c918e55d5c690e4183d1d8
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-02 20:02:24 +0900

    [Gold III] Title: 파티, Time: 324 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,20 +1,20 @@
 import heapq
 N,M,X = map(int,input().split())
-R = [{k: [] for k in range(1,N+1)} for _ in range(2)]
+R = [[[] for _ in range(N+1)] for _ in range(2)]
 for _ in range(M):
     a,b,c = map(int,input().split())
     R[0][a] += [(b,c)]
     R[1][b] += [(a,c)]
-dist = [[0 if n in (0,X) else float("INF") for n in range(N+1)] for _ in range(2)]
+D = [[0 if n in (0,X) else float("INF") for n in range(N+1)] for _ in range(2)]
 for x in range(2):
     Q = []
     heapq.heappush(Q,(0,X))
     while Q:
-        time,node = heapq.heappop(Q)
-        if dist[x][node] >= time:
-            for i in R[x][node]:
-                cost = time + i[1]
-                if cost < dist[x][i[0]]:
-                    dist[x][i[0]] = cost
-                    heapq.heappush(Q,(cost,i[0]))
-print(max(sum(s) for s in zip(*dist)))
+        t,n = heapq.heappop(Q)
+        if D[x][n] >= t:
+            for i,j in R[x][n]:
+                c = t + j
+                if c < D[x][i]:
+                    D[x][i] = c
+                    heapq.heappush(Q,(c,i))
+print(max(sum(s) for s in zip(*D)))
