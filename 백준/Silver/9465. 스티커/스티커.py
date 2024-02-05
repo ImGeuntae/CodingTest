6482c0521ab8d2328a09d9905a3e1f0752489433
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-05 22:52:49 +0900

    [Silver I] Title: 스티커, Time: 620 ms, Memory: 41272 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
     n = int(input())
-    a = b = c = d = 0
+    a = b = 0
     S = [list(map(int,input().split())) for _ in range(2)]
     for x,y in zip(S[0],S[1]):
-        a,b,c,d = c,d,max(a,b,d)+x,max(a,b,c)+y
-    print(max(c,d))
+        a,b, = max(a,b+x),max(b,a+y)
+    print(max(a,b))
