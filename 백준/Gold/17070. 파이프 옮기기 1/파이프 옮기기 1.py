624557e1fc75f9f8c694e754ac2dc8afc665c7b9
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-07 18:53:33 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -5,8 +5,5 @@ dp[0][1][0] = 1
 for i in range(N):
     for j in range(2,N):
         if H[i][j] == '0':
-            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
-            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
-            if H[i][j-1] == H[i-1][j] == '0':
-                dp[i][j][2] = sum(dp[i-1][j-1])
+            dp[i][j] = [sum(dp[i][j-1][:2]),sum(dp[i-1][j-1])*(H[i][j-1]==H[i-1][j]=='0'),sum(dp[i-1][j][1:])]
 print(sum(dp[-1][-1]))
