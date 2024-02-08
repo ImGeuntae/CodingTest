47c73bf9c45c1d15307aee3f841f0936bcf80138
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-08 12:42:59 +0900

    [Gold IV] Title: 문자열 폭발, Time: 400 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,10 +1,7 @@
-import sys
-S = sys.stdin.readline().strip()
-B = [*sys.stdin.readline().strip()]
-n = len(B)
-Q = []
+S,*B = input(),*input()
+n,Q = len(B),[]
 for s in S:
     Q += [s]
     if Q[-n:] == B:
         del Q[-n:]    
-print(''.join(Q) if Q else 'FRULA')
+print(''.join(Q) or 'FRULA')
