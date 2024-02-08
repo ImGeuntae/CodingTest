91d0b5d54de6934c50c65fad2ffbcf4d97c4bdd8
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-08 09:46:52 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -2,12 +2,12 @@ G = {}
 for _ in range(int(input())):
     a,b,c = input().split()
     G[a] = [b,c]
-def X(A,G):
+def X(A):
     a=b=c=d=e=f=''
     if G[A][0] != '.':
-        a,b,c = X(G[A][0],G)
+        a,b,c = X(G[A][0])
     if G[A][1] != '.':
-        d,e,f = X(G[A][1],G)
+        d,e,f = X(G[A][1])
     return [A+a+d,b+A+e,c+f+A]
-print(*X('A',G))
+print(*X('A'))
 
