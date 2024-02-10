61a6abfb1330ce1d424d39e2171656cace74b0ac
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-02-10 16:33:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 2220 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,6 +2,5 @@ N,K = map(int,input().split())
 dp = [0]*(K+1)
 for _ in range(N):
     a,b = map(int,input().split())
-    for x in range(K,a-1,-1):
-        dp[x] = max(dp[x],dp[x-a]+b)
+    dp = [dp[x] if a>x else max(dp[x-a]+b,dp[x]) for x in range(K+1)]
 print(dp[-1])
