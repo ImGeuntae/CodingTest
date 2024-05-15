1e6fb5ea94f3928c250353c4e38ccc67e8543d23
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-05-15 23:02:21 +0900

    [level 2] Title: 양궁대회, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,8 +1,9 @@
 def solution(n, info_A):
+    score_A = sum((i>0)*(10-idx) for idx,i in enumerate(info_A))
     info_L = [0]*11
     arrow_L = [i+1 for i in info_A[:-1]]
-    rate = [[10-index,(2-(hit==1))*(10-index),hit] for index,hit in enumerate(arrow_L)]
-    rate.sort(key=lambda x:x[0])    
+    rate = [[10-idx,(2-(hit==1))*(10-idx),hit] for idx,hit in enumerate(arrow_L)]
+    rate.sort(key=lambda x:x[0])
     dp = [[0,[],11] for _ in range(n+1)]
     for a,b,c in rate:
         for x in range(n,c-1,-1):
@@ -12,6 +13,6 @@ def solution(n, info_A):
                 dp[x][2] = min(dp[x][1])
     for i in dp[n][1]:
         info_L[10-i] = arrow_L[10-i]
-    info_L[-1] = n-sum(info_L)        
-    answer = sum(((l>a)-(a>l))*(10-index) for index,(a,l) in enumerate(zip(info_A,info_L)))
+    info_L[-1] = n-sum(info_L)
+    answer = dp[n][0] - score_A
     return info_L if answer>0 else [-1]  
