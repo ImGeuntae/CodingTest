9b0e8a18240c885b76388e19bb442f35e594671a
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2025-05-03 20:16:45 +0900

    [Gold IV] Title: 알파벳, Time: 2156 ms, Memory: 52888 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -4,7 +4,7 @@ Q = set([(0,0,B[0][0])])
 ans = 0
 while Q:
     a,b,s = Q.pop()
-    for x,y in [(a+1,b,),(a-1,b),(a,b+1),(a,b-1)]:
+    for x,y in [(a+1,b),(a-1,b),(a,b+1),(a,b-1)]:
         if 0<=x<N and 0<=y<M and B[x][y] not in s:
             Q.add((x,y,s+B[x][y]))
         else:
